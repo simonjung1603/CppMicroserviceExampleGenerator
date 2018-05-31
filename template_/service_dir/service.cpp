9d842b2488a7afabdcf977d4dbfb77c4154cbf53
Simon Jung <simonjung1603@gmail.com>
2018-05-31 14:41:49 +0200

    Removed old template

--- template_/service_dir/service.cpp
+++
@@ -1,5 +0,0 @@
-#include "@{service_interface_name}.h"
-
-@{service_interface_name}::~@{service_interface_name}()
-{
-}
--- template_/service_dir/service.h
+++
@@ -1,9 +0,0 @@
-#include <@{service_interface_export_header}.h>
-
-struct @{service_interface_export_macro} @{service_interface_name}
-{
-  virtual ~@{service_interface_name}();
-
-  // Define your interface
-  virtual void Ping() const = 0;
-};
